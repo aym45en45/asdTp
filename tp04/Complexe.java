4df5f4d56b41ca0cc5b5930baba990adac4e1c42
aymen rouissa <aymendev45@gmail.com>
2023-12-02 18:07:00 +0100

    rigalt tp4
    'nahit static' 'wnahit Tab mn class Complexe'

--- tp04/Complexe.java
+++ tp04/Complexe.java
@@ -1,34 +1,34 @@
 public abstract class Complexe {
 
-    public static void AfficherSomme(ComplexeTab c1, ComplexeTab c2) {
-        System.err.println("la somme " + somme(c1, c2).rl() + " + " + somme(c1, c2).img() + "i");
+    public void AfficherSomme(Complexe c1) {
+        System.err.println("la somme " + somme(c1, this).rl() + " + " + somme(c1, this).img() + "i");
     }
 
-    public static void AfficherSoustraction(ComplexeTab c1, ComplexeTab c2) {
-        System.out.println("La soustraction " + soustraction(c1, c2).rl() + " + " + soustraction(c1, c2).img() + "i");
+    public void AfficherSoustraction(Complexe c1) {
+        System.out.println("La soustraction " + soustraction(c1, this).rl() + " + " + soustraction(c1, this).img() + "i");
     }
 
-    public static Complexe somme(ComplexeTab c1, ComplexeTab c2) {
+    public Complexe somme(Complexe c1, Complexe c2) {
         float rl = c1.rl() + c2.rl();
         float img = c1.img() + c2.img();
-        return c1.makeC(rl, img);
+        return makeC(rl, img);
     }
 
-    public static Complexe soustraction(ComplexeTab c1, ComplexeTab c2) {
+    public Complexe soustraction(Complexe c1, Complexe c2) {
         float rl = c1.rl() - c2.rl();
         float img = c1.img() - c2.img();
-        return c1.makeC(rl, img);
+        return makeC(rl, img);
     }
 
-    private static double m(ComplexeTab c) {
+    private double m(Complexe c) {
         return Math.sqrt(c.rl() * c.rl() + c.img() * c.img());
     }
 
-    public static void mudel(ComplexeTab c1, ComplexeTab c2) {
-        if (m(c1) > m(c2))
+    public void mudel(Complexe c1) {
+        if (m(c1) > m(this))
             System.err.println("le plus grand est " + c1.rl() + " + " + c1.img());
         else
-            System.err.println("le plus grand est " + c2.rl() + " + " + c2.img());
+            System.err.println("le plus grand est " + this.rl() + " + " + this.img());
 
     }
 
--- tp04/Main.java
+++ tp04/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         ComplexeTab tabC = new ComplexeTab(3, -9);
         ComplexeTab tabC1 = new ComplexeTab(8, 3);
-        Complexe.AfficherSomme(tabC,tabC1);
-        Complexe.AfficherSoustraction(tabC,tabC1);
-        Complexe.mudel(tabC,tabC1);
+        tabC.AfficherSomme(tabC1);
+        tabC.AfficherSoustraction(tabC1);
+        tabC.mudel(tabC1);
     }
 }
